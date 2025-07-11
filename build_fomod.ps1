0bb0f4202339289a8f24499159ba997a0f6fc305
strangerism <massimovirgilio@hotmail.com>
2025-07-11 19:44:15 +0200

    first commit

---
+++ build_fomod.ps1
@@ -0,0 +1,27 @@
+
+
+function CreateBuildFolders {
+    
+    New-Item -Path "build" -ItemType Directory | Out-Null
+
+    New-Item -Path "build/SEALs" -ItemType Directory | Out-Null
+    
+}
+
+function BuildMod {
+
+    $target = "build/SEALs"
+    Copy-Item -Recurse -Force -Path ".\Main\gamedata" -Destination $target -Exclude .bak
+}
+
+CreateBuildFolders
+BuildMod
+
+$compress = @{
+    Path = "build/SEALs/gamedata" 
+    CompressionLevel = "Fastest"
+    DestinationPath = "release/SEALs.zip"
+}
+Compress-Archive @compress -Force
+
+Remove-Item -Force -Recurse -Path "./build"
